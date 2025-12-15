2fd564edde727f783af64eaec18f1e08c4838e9c
Eden <edenth0610@gmail.com>
2025-12-15 19:00:32 +0100

    Delete smiling-cereals directory

--- smiling-cereals/target/classes/import.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE users ALTER COLUMN id SET DEFAULT nextval('users_seq');
-ALTER TABLE credentials ALTER COLUMN id SET DEFAULT nextval('credentials_seq');
-
-INSERT INTO users(name, surname, email)VALUES('Admin', 'Root', 'admin@mail.com');
-
-
-INSERT INTO credentials(username, password, role, user_id)VALUES('admin', '$2a$10$zXfQs9TdcOamfi6oBuKZqu8miF/LV93/CwqBlRdNWR8yRdsd14I1i', 'ADMIN', 1);
